5e3d69620fdd4243e553e65ecb2cfda7bb81e9f6
Miiller caetano do amaral <miilleramaral@gmail.com>
2017-12-19 22:34:50 -0200

    Text_Example

---
+++ Text_Example/Text_Example/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace Text_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region StringBuilder
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("New");
+            sb.Append("York");
+
+            sb.Insert(5, " HELLO ");
+            sb.Remove(5, 6);
+            sb.Replace('k', 'q');
+
+
+            Console.Write(sb);
+            sb.Clear();
+            Console.ReadKey();
+            #endregion
+        }
+    }
+}
